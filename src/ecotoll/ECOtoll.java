ceeaa52e08858b5b5a6c3b36c494fd2bc4ad0af3
BryantMichelleSarabiaOrtega <48278923+BryantMichelleSarabiaOrtega@users.noreply.github.com>
2019-04-14 22:02:08 +0200

    Update ECOtoll.java

--- src/ecotoll/ECOtoll.java
+++ src/ecotoll/ECOtoll.java
@@ -12,7 +12,7 @@ public class ECOtoll {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        veicolo macchina = new veicolo("Punto","Fiat","BE102E",2,1998,1.25f,1.50f);
+        Classe_A macchina = new Classe_A("Punto","Fiat","BE102E",2,1998,1.25f,1.50f);
         Autostrada A14 = new Autostrada("A50",false);
         Casello c1 = new Casello("Colledara",250);
         Casello c2 = new Casello("Teramo",312);
@@ -30,7 +30,7 @@ public class ECOtoll {
         A14.addTariffa(taf4);
         A14.addTariffa(taf5);
         double costo = p1.getCosto(macchina, A14, "Colledara", "Teramo");
-        System.out.println(costo);
+        System.out.println("€" + costo);
 
     }
     
